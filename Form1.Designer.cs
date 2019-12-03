a17ac9419b1c279fcc5767e658383de4a43969e6
Shiann <shiann.baker@trojans.dsu.edu>
2019-12-03 15:44:06 -0600

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace PacMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.blue = new System.Windows.Forms.PictureBox();
+            this.pac = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pac)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // blue
+            // 
+            this.blue.Image = ((System.Drawing.Image)(resources.GetObject("blue.Image")));
+            this.blue.Location = new System.Drawing.Point(280, 143);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(45, 45);
+            this.blue.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.blue.TabIndex = 4;
+            this.blue.TabStop = false;
+            // 
+            // pac
+            // 
+            this.pac.Image = ((System.Drawing.Image)(resources.GetObject("pac.Image")));
+            this.pac.Location = new System.Drawing.Point(280, 204);
+            this.pac.Name = "pac";
+            this.pac.Size = new System.Drawing.Size(39, 39);
+            this.pac.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pac.TabIndex = 6;
+            this.pac.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(12, 588);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 35);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Score: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 331);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(602, 333);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pac);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Key_Down);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Key_Up);
+            ((System.ComponentModel.ISupportInitialize)(this.blue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pac)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox blue;
+        private System.Windows.Forms.PictureBox pac;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacMan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Key_Down(object sender, KeyEventArgs e)//movement w/ bounds
+        {
+            int x, y, d;
+            d = 0;
+            x = pac.Location.X;
+            y = pac.Location.Y;
+
+            if (e.KeyCode == Keys.Down) { y += 3; d = 1; }
+            if (e.KeyCode == Keys.Up) { y -= 3; d = 2; }
+            if (e.KeyCode == Keys.Left) { x -= 3; d = 3; }
+            if (e.KeyCode == Keys.Right) { x += 3; d = 4; }
+
+            if ( check_direction(x, y, d))
+            {
+                pac.Location = new Point(x, y);
+                //call New_Score send in location
+            }
+        }
+
+        private void Key_Up(object sender, KeyEventArgs e)
+        {
+        }
+
+        bool check_direction(int _x, int _y, int _d)
+        {
+            if (_x > 18 && _x < 405 && _y < 218 && _y > 21)
+            {
+                if (_d == 1 || _d == 2)//vertical
+                {
+                    if (_x > 130 && _x < 133) //left of pen
+                        return true;
+                    else if (_x > 290 && _x < 293) //right of pen
+                        return true;
+                    else if (_x > 65 && _x < 68 && _y > 69 && _y < 169 || _x > 354 && _x < 358 && _y > 69 && _y < 169) //left inside rec || right inside rec
+                        return true;
+                    else if (_x < 23 || _x > 399 && _x < 405) //far left || far right
+                        return true;
+                    else if (_x > 86 && _x < 90 && _y < 72 || _x > 86 && _x < 90 && _y > 163) //left small verticals
+                        return true;
+                    else if (_x > 330 && _x < 339 && _y < 72 || _x > 330 && _x < 339 && _y > 163) //right small verticals
+                        return true;
+                    else
+                        return false;
+                }
+                else if (_d == 3 || _d == 4)//horizontal
+                {
+                    if (_y < 169 && _y > 163 && _x > 63 && _x < 358 || _y < 72 && _y > 69 && _x > 63 && _x < 358) //below pen || above pen
+                        return true;
+                    else if (_x > 130 && _x < 293 && _y > 215 || _x > 130 && _x < 293 && _y < 24) //bottom inside box || top inside box
+                        return true;
+                    else if (_y > 117 && _y < 121 && _x > 290 || _y > 117 && _y < 121 && _x < 133) //middle right of pen || left
+                        return true;
+                    else if (_x < 88 && _y < 24 || _x > 330 && _y < 24) //very top left || right
+                        return true;
+                    else if (_x < 88 && _y > 215 || _x > 330 && _y > 215) //very bottom
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                    return false;
+            }
+            else
+                return false;
+        }
+    }
+
+   /* public class Score
+    {
+        int score = 0;
+
+        public void New_Score(object pac)
+        {
+            int x = pac.Location.X;
+        }
+    }*/
+}
